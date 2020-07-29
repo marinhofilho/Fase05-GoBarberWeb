1af674ed68716495e7deeae9c02e9e99c133bc1d
marinhofilho <mariolemanf@gmail.com>
2020-07-29 19:54:04 -0300

    GoBarberWeb - Fase05 - first commit

---
+++ src/styles/global.js
@@ -0,0 +1,44 @@
+import { createGlobalStyle } from 'styled-components';
+
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    background: linear-gradient(-90deg, #7159c1, #ab59c1);
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul { 
+    list-style: none;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+
+`;
